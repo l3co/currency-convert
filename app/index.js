2097b310411c3589e4b1b95335b4c26a062da839
leandro costa <lcosta.sp.br@gmail.com>
2022-07-07 11:26:37 -0300

    feat(currency): :construction: create endpoint to work with currency
    closes #5

--- app/index.js
+++ app/index.js
@@ -51,6 +51,21 @@ app.put('/user/:id', (req, res) => {
     return res.status(200).json({ id: id, ...user })
 })
 
+app.post('/currency', (req, res) => {
+    return res
+        .status(200)
+        .json({
+            id: 1,
+            user_id: 1,
+            source_currency: 'BRL',
+            destination_currency: 'USD',
+            input_value: 5.24,
+            output_value: 1,
+            tax: '1%',
+            datetime: `${Date.now()}`,
+        })
+})
+
 app.listen(port, () => {
     console.log(`Application is running ${port}`);
 })
