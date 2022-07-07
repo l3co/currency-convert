af23d3521a3febb4756548f85d1e57671b844c70
leandro costa <lcosta.sp.br@gmail.com>
2022-07-07 11:18:01 -0300

    feat(user): :construction: create endpoints to work with user
    closes #6

--- app/index.js
+++ app/index.js
@@ -10,9 +10,45 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.json({
+    return res.json({
         "message": "Hello"
     })
+})
+
+app.get('/user', (req, res) => {
+    return res.json([
+        {
+            id: 1,
+            name: 'Leandro Costa',
+            email: 'lcosta.sp.br@gmail.com'
+        }
+    ])
+})
+
+app.get('/user/:id', (req, res) => {
+    const { id } = req.params
+    return res.json({
+        id: id,
+        name: 'Leandro Costa',
+        email: 'lcosta.sp.br@gmail.com'
+    })
+})
+
+app.post('/user', (req, res) => {
+    const { user } = req.body
+    return res.status(201).json(user)
+})
+
+app.delete('/user/:id', (req, res) => {
+    const { id } = req.params
+    return res.status(204).send()
+})
+
+app.put('/user/:id', (req, res) => {
+    const { id } = req.params
+    const { user } = req.body
+    console.log(user);
+    return res.status(200).json({ id: id, ...user })
 })
 
 app.listen(port, () => {
