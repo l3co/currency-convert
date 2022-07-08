e0e7adcdbad34801cc2a8e7b198d268ebe6f45f2
leandro costa <lcosta.sp.br@gmail.com>
2022-07-08 18:34:07 -0300

    refactor(user): :art: segregate structure user service and repository

--- src/main/app/controllers/UserController.js
+++ src/main/app/controllers/UserController.js
@@ -1,10 +1,10 @@
-const service = require('../services/UserService')
+const { UserService } = require('../services')
 
 class UserController {
 
     static async listAll(_, res) {
         try {
-            const users = await service.listAll()
+            const users = await UserService.listAll()
             return res.json(users)
         } catch (err) {
             return res.json({ message: err.message })
@@ -14,7 +14,7 @@ class UserController {
     static async findByID(req, res) {
         const { id } = req.params
         try {
-            const user = await service.findByID(id)
+            const user = await UserService.findByID(id)
             return user ? res.json(user) : res.status(404).json({ message: 'User not found' })
         } catch (err) {
             return res.status(400).json({ message: err.message })
@@ -24,7 +24,7 @@ class UserController {
     static async create(req, res) {
         try {
             const user = req.body
-            await service.create(user)
+            await UserService.create(user)
             return res.status(201).send()
         } catch (err) {
             return res.status(400).json({ message: err.message })
@@ -35,7 +35,7 @@ class UserController {
         const { id } = req.params
         const user = req.body
         try {
-            const userResponse = await service.update(id, user)
+            const userResponse = await UserService.update(id, user)
             return res.status(200).json(userResponse)
         } catch (error) {
             return res.status(400).json({ message: 'Error to update user' })
@@ -45,7 +45,7 @@ class UserController {
     static async delete(req, res) {
         try {
             const { id } = req.params
-            await service.delete(id)
+            await UserService.delete(id)
             return res.status(204).send()
         } catch (error) {
             return res.status(400).json({ message: 'Error to delete user' })
