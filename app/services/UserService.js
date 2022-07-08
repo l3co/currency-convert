34e515f23f5d237ca876b8dbe2eab35e47f1f846
leandro costa <lcosta.sp.br@gmail.com>
2022-07-07 23:57:50 -0300

    feat(user): :sparkles: create user service
    closes #8

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -5,6 +5,24 @@ class UserService {
     static async listAll() {
         return await User.findAll()
     }
+
+    static async findByID(id) {
+        return await User.findOne({
+            where: { id: id }
+        })
+    }
+
+    static async create(user) {
+        return await User.create(user)
+    }
+
+    static async update(id, user) {
+        return await User.update(user, { where: { id: id } })
+    }
+
+    static async delete(id) {
+        return await User.destroy({ where: { id: id } })
+    }
 }
 
 module.exports = UserService
